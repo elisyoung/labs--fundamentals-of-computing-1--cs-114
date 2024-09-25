4948285593ce12a16b80e1b958fb65a070801541
elisyoung <eyoung@hartford.edu>
2024-09-25 16:08:44 -0400

    added third column

--- lab-01/Names.java
+++ lab-01/Names.java
@@ -6,10 +6,10 @@
 public class Names {
   public static void main(String[] args) {
     System.out.println();
-    System.out.println(" \tName\t\tHometown");
-    System.out.println("\t====\t\t========");
-    System.out.println("\tSally\t\tRoanoke");
-    System.out.println("\tAlexander\tWashington");
+    System.out.println("\tName\t\tHometown\t\tMajor");
+    System.out.println("\t====\t\t========\t\t=======");
+    System.out.println("\tSally\t\tRoanoke\t\t\tCS");
+    System.out.println("\tAlexander\tWashington\t\tMusic");
     System.out.println();
   }
 }
