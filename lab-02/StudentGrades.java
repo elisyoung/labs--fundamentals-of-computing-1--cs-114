cef838df0bb4771ace82a88e8caa43212dd74067
elisyoung <eyoung@hartford.edu>
2024-09-25 16:25:17 -0400

    Need to figure out slashes

--- lab-02/StudentGrades.java
+++ lab-02/StudentGrades.java
@@ -6,5 +6,14 @@
 
 public class StudentGrades {
   public static void main(String[] args) {
+    System.out.println();
+    System.out.println(" ==\t\tStudent Points\t\t==");
+    System.out.println();
+    System.out.println("Name\t\tLab\tPoints\tTotal");
+    System.out.println("Joe\t\t43\t7\t"+(43+7));
+    System.out.println("William\t\t50\t8\t"+(50+8));
+    System.out.println("Mary Sue\t39\t10\t"+(39+10));
+    System.out.println("Amanda\t\t59\t1\t"+(59+1));
+    System.out.println("Chris\t\t48\t6\t"+(48+6));
+    }
   }
-}
