82ab0a12aa8fa43fa5e15fc1ef786173d9882505
elisyoung <eyoung@hartford.edu>
2024-09-25 16:38:57 -0400

    completed

--- lab-02/Paint.java
+++ lab-02/Paint.java
@@ -9,6 +9,20 @@ import java.util.Scanner;
 public class Paint {
   public static void main(String[] args) {
     final int COVERAGE = 350;  //paint covers 350 sq ft/gal
+    int length, width, height;
+    double totalSqFt, paintNeeded;
+
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the length of the room: ");
+    length = scan.nextInt();
+    System.out.print("Enter the width of the room: ");
+    width = scan.nextInt();
+    System.out.print("Enter the height of the room: ");
+    height = scan.nextInt();
+    totalSqFt = (2 * length * height)+(2 * width * height);
+    paintNeeded = totalSqFt / COVERAGE;
+    System.out.println("A " + length + "x" + width + "x" + height + " room requires " + paintNeeded + " gallons of paint");
+
       // Declare integers length, width, and height.
       // Declare double totalSqFt;
       // Declare double paintNeeded;
