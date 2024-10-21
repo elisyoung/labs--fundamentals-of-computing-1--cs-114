a62ab2a65b8bb8a903924cd3e11342de00a69f90
elisyoung <eyoung@hartford.edu>
2024-10-20 21:22:10 -0400

    printed powers of 2

--- lab-04/PowersOf2.java
+++ lab-04/PowersOf2.java
@@ -13,15 +13,20 @@ public class PowersOf2 {
     numPowersOf2 = scan.nextInt();
 
     // print a message saying how many powers of 2 will be printed
+    System.out.println(numPowersOf2 + " will be printed");
+
     // initialize exponent -- the first thing printed is 2 to the what?
     exponent = 0;
 
-    while ( ) {
+    while (exponent <= numPowersOf2) {
       //print out current power of 2
+      System.out.println("2^" + exponent + "=" + nextPowerOf2);
 
       //find next power of 2 -- how do you get this from the last one?
+      nextPowerOf2 *= 2;
 
       //increment exponent
+      exponent++;
     }
   }
 }
