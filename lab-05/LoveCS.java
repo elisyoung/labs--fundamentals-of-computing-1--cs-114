38602ad460165800a1fc39cf68f2cf37cc8a13f4
elisyoung <eyoung@hartford.edu>
2024-10-20 21:30:12 -0400

    printed values and sum

--- lab-05/LoveCS.java
+++ lab-05/LoveCS.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class LoveCS {
   public static void main(String[] args) {
-    final int LIMIT = 10;
+    int max, count = 1, sum = 0;
 
-    int count = 1;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    max = input.nextInt();
 
-    while (count <= LIMIT) {
-      System.out.println("I love Computer Science!!");
+    System.out.println();
 
+    while (count <= max) {
+      System.out.println(count + ". I love Computer Science!!");
+      sum += count;
       count++;
     }
+
+    System.out.println("the message was printed " + (count - 1) + " times");
+    System.out.println("the sum of the values is " + sum);
   }
 }
