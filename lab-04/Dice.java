65061591007d23cb4b43f77a5d66bc5459cc02f7
elisyoung <eyoung@hartford.edu>
2024-10-20 21:16:24 -0400

    rolled two rice and displayed sum

--- lab-04/Dice.java
+++ lab-04/Dice.java
@@ -8,5 +8,14 @@ import java.util.Random;
 
 public class Dice {
   public static void main(String[] args) {
+    Random generator = new Random();
+    int die1, die2;
+
+    die1 = generator.nextInt(6) + 1;
+    die2 = generator.nextInt(6) + 1;
+
+    System.out.println("the first roll is " + die1);
+    System.out.println("the second roll is " + die2);
+    System.out.println("the sum of the two rolls is " + (die1 + die2));
   }
 }
