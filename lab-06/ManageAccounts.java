dba72d416457e9fbcc6035dd46e36f5ca9912031
elisyoung <eyoung@hartford.edu>
2024-10-20 21:57:30 -0400

    filled in manage account program

--- lab-06/ManageAccounts.java
+++ lab-06/ManageAccounts.java
@@ -12,19 +12,29 @@ public class ManageAccounts {
     acct1 = new Account(1000, "Sally", 1111);
 
     // Create account2 for Joe with $500
+    acct2 = new Account(500, "Joe", 2222);
 
     // Deposit $100 to Joe’s account
+    acct2.deposit(100);
 
     // Print Joe’s new balance (use getBalance())
+    acct2.getBalance();
 
     // Withdraw $50 from Sally’s account
+    acct2.withdraw(50);
 
     // Print Sally’s new balance (use getBalance())
+    acct2.getBalance();
 
     // Charge fees to both accounts
+    acct2.chargeFee();
+    acct1.chargeFee();
 
     // Change the name on Joe’s account to Joseph
+    acct2.changeName("Joseph");
 
     // Print summary for both accounts
+    acct1.getBalance();
+    acct2.getBalance();
   }
 }
